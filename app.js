68f4b60012fc573cf5adde50ac9b6ecc9ce02578
Lukian <lulul7018@gmail.com>
2023-06-20 15:25:19 +0200

    commit

--- app.js
+++ app.js
@@ -1,40 +1,17 @@
 //Importing libs
-const { Telegraf } = require('telegraf');
 const discord = require('discord.js');
 
 //Importing events
 const discordEvents = require('./events/discordEvents'); 
-const telegramEvents = require('./events/telegramEvents');
 
 //bot initialization
-const bot = new Telegraf(process.env.TELEGRAM);
 const client = new discord.Client({intents: 33297});
 
-//Telegram events
-telegramEvents.start(bot);
-telegramEvents.help(bot);
-telegramEvents.github(bot);
-telegramEvents.truce(bot);
-telegramEvents.chatinfo(bot);
-telegramEvents.suggest(bot);
-telegramEvents.rtag(bot);
-telegramEvents.r34(bot);
-telegramEvents.dadjoke(bot);
-telegramEvents.g(bot);
-telegramEvents.q(bot);
-telegramEvents.quota(bot);
-telegramEvents.lc(bot);
-telegramEvents.addconv(bot);
-telegramEvents.delconv(bot);
-telegramEvents.displayconv(bot);
-telegramEvents.addmsg(bot);
-
-//Discord events
+//events
 discordEvents.ready(client);
 discordEvents.newMessage(client);
 discordEvents.newInteraction(client);
 discordEvents.guildCreate(client);
 
 //bot launch
-bot.launch()
 client.login(process.env.DISCORD);
